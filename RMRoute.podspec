26be43eb3c0caa690f77433a82677ca15ad4a581
Niels Koole <niels.koole@soundofdata.nl>
2016-12-08 09:54:02 +0100

    Initial release

---
+++ RMRoute.podspec
@@ -0,0 +1,11 @@
+Pod::Spec.new do |spec|
+  spec.name             = 'RMRoute'
+  spec.version          = '0.1'
+  spec.license          = { :type => 'BSD' }
+  spec.homepage         = 'https://github.com/roadmap-travel/RMRoute'
+  spec.authors          = { 'Niels Koole' => 'niels@getroadmap.com' }
+  spec.summary          = 'A lightweight implementation to use routes in your application.'
+  spec.source           = { :git => 'https://github.com/roadmap-travel/RMRoute.git', :tag => '0.1' }
+  spec.source_files     = 'Source/*'
+  spec.requires_arc     = true
+end
---
+++ RMRoute/Source/RMRoute.swift
@@ -0,0 +1,94 @@
+//
+//  Routes.swift
+//  Roadmap
+//
+//  Created by Niels Koole on 07/12/16.
+//  Copyright © 2016 Roadmap. All rights reserved.
+//
+
+import Foundation
+
+@objc enum RMRouteAnimation: Int {
+	case push = 0
+	case present = 1
+}
+
+class RMRoute: NSObject {
+	
+	private static let shared = RMRoute()
+	
+	var registry:[String:(UIViewController, RMRouteAnimation, [AnyObject]) -> Void] = [:]
+	
+	func register(path: String, action: (UIViewController, RMRouteAnimation, [AnyObject]) -> Void) {
+		// Save route to registry
+		registry[path] = action
+	}
+	
+	private func navigate(to: String, delegate: UIViewController, animation: RMRouteAnimation) -> Bool {
+		
+		// Get routes with equal components
+		let toComponents = to.componentsSeparatedByString("/")
+		let filteredRoutes = registry.filter { return $0.0.componentsSeparatedByString("/").count == toComponents.count }
+		
+		for route in filteredRoutes {
+			let routeComponents = route.0.componentsSeparatedByString("/")
+			var parameters = [String]()
+			
+			for i in 0 ..< routeComponents.count {
+				let toComponent = toComponents[i]
+				let routeComponent = routeComponents[i]
+				
+				if routeComponent.hasPrefix("{") {
+					parameters.append(toComponent)
+				}
+				
+				// Equalize the route
+				if routeComponent.hasPrefix("{") == false && toComponent != routeComponent {
+					// The components are not equal, please try next route
+					break
+				} else if i == routeComponents.count - 1 {
+					// Last component -> Route found
+					let action = route.1
+					
+					// Execute action handlers with
+					action(delegate, animation, parameters)
+					
+					return true
+				}
+			}
+		}
+		
+		// Route not found
+		return false
+	}
+	
+	// MARK: - Public accessor
+	
+	@objc static func register(path: String, action: (UIViewController, RMRouteAnimation, [AnyObject]) -> Void) {
+		return RMRoute.shared.register(path, action: action)
+	}
+	
+	static func navigate(to: String, delegate: UIViewController, animation: RMRouteAnimation) -> Bool {
+		return RMRoute.shared.navigate(to, delegate: delegate, animation: animation)
+	}
+}
+
+extension UIViewController {
+	
+	func navigate(to: String, animation: RMRouteAnimation) {
+		RMRoute.navigate(to, delegate: self, animation: animation)
+	}
+	
+	func animate(to: UIViewController, animation: RMRouteAnimation) {
+		switch animation {
+		case .push:
+			self.navigationController?.pushViewController(to, animated: true)
+			break
+		case .present:
+			let nc = RMNavigationController(rootViewController: to)
+			self.presentViewController(nc, animated: true, completion: nil)
+			break
+		}
+	}
+}
+
