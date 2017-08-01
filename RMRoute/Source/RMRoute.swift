0cc04ff47d87c0d0313a574b64f84e2c557614ba
Paul van Roosendaal <paul@getroadmap.com>
2017-08-01 12:30:19 +0200

    Added support for use of URLs

--- RMRoute/Source/RMRoute.swift
+++ RMRoute/Source/RMRoute.swift
@@ -79,6 +79,62 @@ public class RMRoute: NSObject {
 		return false
 	}
 	
+	
+	private func navigate(to: URL, delegate: UIViewController, animation: RMRouteAnimation) -> Bool {
+		
+		// Use some unusual string - with characters allowed in URLs - to parameterize a url
+		let separator = "---"
+		
+		let toComponents = to.pathComponents
+		
+		let filteredRoutes = registry.filter {
+			let url = URL(string: $0.0.replacingOccurrences(of: "[{}]", with: separator, options: .regularExpression))
+			return url?.pathComponents.count == toComponents.count
+		}
+		
+		for route in filteredRoutes {
+			let url = URL(string: route.0.replacingOccurrences(of: "[{}]", with: separator, options: .regularExpression))
+			guard let routeComponents = url?.pathComponents else {
+				return false
+			}
+			
+			var parameters = [String]()
+			
+			for i in 0 ..< routeComponents.count {
+				let toComponent = toComponents[i]
+				let routeComponent = routeComponents[i]
+				
+				//If the component starts with `separator` consider it to be a dynamic property and add it as param
+				if routeComponent.hasPrefix(separator) {
+					parameters.append(toComponent)
+				}
+				
+				// Equalize the route
+				// Skip it if it contains 'separator', because those components are dynamic and thus never equal
+				if routeComponent.contains(separator) == false && toComponent.lowercased() != routeComponent.lowercased() {
+					// The components are not equal, please try next route
+					break
+				} else if i == routeComponents.count - 1 {
+					
+					to.queryItems?.forEach() { (item) in
+						if let val = item.value {
+							parameters.append(val)
+						}
+					}
+					
+					// Last component -> Route found
+					let action = route.1
+					
+					// Execute action handlers with
+					return action(delegate, animation, parameters)
+				}
+			}
+		}
+		
+		// Route not found
+		return false
+	}
+	
 	// MARK: - Public accessor
 	
 	public static func register(path: String, action: @escaping (UIViewController, RMRouteAnimation, [String]) -> Bool) {
@@ -86,6 +142,11 @@ public class RMRoute: NSObject {
 	}
 	
 	public static func navigate(to: String, delegate: UIViewController, animation: RMRouteAnimation) -> Bool {
+		return RMRoute.shared.navigate(to: to, delegate: delegate, animation: animation)
+	}
+	
+	@objc(navigatetoURL:delegate:animation:)
+	public static func navigate(to: URL, delegate: UIViewController, animation: RMRouteAnimation) -> Bool {
 		return RMRoute.shared.navigate(to: to, delegate: delegate, animation: animation)
 	}
 }
@@ -115,3 +176,13 @@ public extension UIViewController {
 	}
 }
 
+public extension URL {
+	
+	public var queryItems:[URLQueryItem]? {
+		guard let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+			let queryItems = components.queryItems else {
+				return nil
+		}
+		return queryItems
+	}
+}
